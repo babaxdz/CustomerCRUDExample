1c318e351718f3d6b57c8ec85bb2ee06c09b7c29
24705 <24705@24705-437646.telxsi.com>
2021-04-12 18:52:01 +0530

    third commit

--- src/main/java/tel/example/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package tel.example;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-	
-	public Customer findByName(String name);
-
-}
